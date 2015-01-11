f3f364cfd13c7af7338d34f560e304f883dd89b9
hu.xiongfei <hu.xiongfei@topka.cn>
2015-01-11 19:55:06 +0800

    添加 dingo/api

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Cartalyst\Sentry\SentryServiceProvider',
+		'Dingo\Api\Provider\ApiServiceProvider',
 
 
 	),
@@ -190,7 +191,8 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
-		'Sentry' => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
+		'Sentry' 			=> 'Cartalyst\Sentry\Facades\Laravel\Sentry',
+		'API' 				=> 'Dingo\Api\Facade\API',
 
 	),
 
